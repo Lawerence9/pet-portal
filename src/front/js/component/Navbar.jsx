6359a64de9e3e8db5893bebc3221e02676d0bf70
Annie Góngora <160527392+AnnieyG@users.noreply.github.com>
2025-04-11 17:49:12 +0000

    navbar

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
-
+import logo from "../../img/logo.jpeg";
 
 export const Navbar = () => {
 	const {store, actions} = useContext(Context)
@@ -15,49 +15,55 @@ export const Navbar = () => {
         navigate("/login")
 	}
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-expand-lg navbar-dark light-dark py-2">
 			<div className="container">
-
+                
+			    <Link to="/">
+				    <span className="h-full object-contain" style={{ padding: 0 }}>
+					<img alt="" src={logo} style={{height: "6.9rem", verticalAlign: "middle", marginRight: "0" }} />
+					</span>
+				
+				</Link>
 				<Link to="/home">
-					<span className="navbar-brand mb-0 h1">HOME</span>
+					<span className="navbar-brand mb-0 h1 nav-link-medio">HOME</span>
 				</Link>
 
 				<Link to="/noticias">
-					<span className="navbar-brand mb-0 h1">NOTICIAS</span>
+					<span className="navbar-brand mb-0 h1 nav-link-medio">NOTICIAS</span>
 				</Link>
 
 				<Link to="/protectoras">
-					<span className="navbar-brand mb-0 h1">PROTECTORAS</span>
+					<span className="navbar-brand mb-0 h1 nav-link-medio">PROTECTORAS</span>
 				</Link>
 
 				<Link to="/veterinarias">
-					<span className="navbar-brand mb-0 h1">VETERINARIAS</span>
+					<span className="navbar-brand mb-0 h1 nav-link-medio">VETERINARIAS</span>
 				</Link>
 
 				<Link to="/adopciones">
-					<span className="navbar-brand mb-0 h1">ADOPCIONES</span>
+					<span className="navbar-brand mb-0 h1 nav-link-medio">ADOPCIONES</span>
 				</Link>
 				
 				<Link to="/sos-animal">
-					<span className="navbar-brand mb-0 h1">SOS</span>
+					<span className="navbar-brand mb-0 h1 nav-link-medio">SOS</span>
 				</Link>
 
 				<Link to="/tiendas">
-					<span className="navbar-brand mb-0 h1">TIENDAS</span>
+					<span className="navbar-brand mb-0 h1 nav-link-medio">TIENDAS</span>
 				</Link>
 
 				<Link to="/donaciones">
-					<span className="navbar-brand mb-0 h1">DONACIONES</span>
+					<span className="navbar-brand mb-0 h1 nav-link-medio">DONACIONES</span>
 				</Link>
 
 				<div className="ml-auto">
 					<Link to="/sign-up">
-						<button className="btn btn-primary">SIGN UP</button>
+						<button className="btn btn-secondary">SIGN UP</button>
 					</Link>
 				</div>
 
 				<div className="ml-auto">
-				{store.isLogged == true ? <span type="button" onClick={handleLogoutButton} className="btn btn-primary">LOGOUT</span> : <Link to="/login"><span className="btn btn-primary">LOGIN</span></Link>}
+				{store.isLogged == true ? <span type="button" onClick={handleLogoutButton} className="btn btn-secondary">LOGOUT</span> : <Link to="/login"><span className="btn btn-secondary">LOGIN</span></Link>}
 				</div>
 			</div>
 		</nav>
