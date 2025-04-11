9330797e5aedc5dfda57afeccddc2bf3f2c5e39b
hectormillan <70292559+hectormillan@users.noreply.github.com>
2025-04-11 05:13:47 +0000

    Ajutes: filtro de ciudad , formulario de correo

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import injectContext from "./store/appContext.js";
 // Custom components
 import ScrollToTop from "./component/ScrollToTop.jsx";
@@ -30,7 +31,8 @@ import { NewAdoption } from "./pages/NewAdoption.jsx";
 import { AdoptionDetails } from "./pages/AdoptionDetails.jsx";
 import { ContactForm } from "./pages/ContactForm.jsx";
 
-const basename = process.env.BASENAME || "";
+const basename = process.env.BASENAME || "home";
+console.log(basename);
 
 //create your first component
 const Layout = () => {
@@ -39,14 +41,15 @@ const Layout = () => {
   
 
     // if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
-    
+ 
 
     return (
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
-                    <Routes>
+                     <Routes>
+                        <Route path="/" element={<Navigate to="/home" replace />} />
                         <Route element={<Home />} path="/home" />
                         <Route element={<Error404 />} path="*" />
                         <Route element={<Adopciones />} path="/adopciones" />
--- src/front/js/pages/Protectoras.jsx
+++ src/front/js/pages/Protectoras.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useSyncExternalStore } from "react";
+import React, { useContext, useEffect, useState, useSyncExternalStore } from "react";
 import { Context } from "../store/appContext.js";
 import { Link, useNavigate, useLocation, Navigate  } from "react-router-dom";
 import Fondopr from "../../img/fprotectora.jpeg";
@@ -8,7 +8,13 @@ export const Protectoras = () => {
 
 	const host = process.env.BACKEND_URL;
 	const rutaImagenes = host + "";
+	const [filter, SetFilter] = useState("");
 	console.log(store.animalShelterSelected)
+
+	const changeFilter = (filter) => {
+
+		SetFilter(filter);
+	}
 		
 
 	const listarAnimalShelters = () => {
@@ -36,38 +42,57 @@ export const Protectoras = () => {
 		<div className="container" style={{backgroundImage: `url(${Fondopr})`, backgroundSize: "cover", height: "200vh"}}>
 			<h1 className="text-center my-4">PROTECTORAS</h1>
 
+			<div className="btn-group my-2">
+						<button class="btn btn-primary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Selecciona Ciudad
+						</button>
+						<ul className="dropdown-menu">
+						{store.animalShelter.map((iterator, index) => (
+								<div key={index}>
+									{/* Aquí va lo que quieras renderizar por cada 'iterator' */}
+									<li key={index}><a className="dropdown-item" href="#" onClick={() => changeFilter(iterator.city)}>{iterator.city}</a></li>
+								</div>
+						))}
+						</ul>
+			</div>
+
 		<div className="row">
-		  
+
+					
+					
 
 					  
 				{/* recorre el array contact usando la función map(); */}  
 				{/* loop through the contact array using the map() function; */}
-				{store.animalShelter.map((iterator, index) =>
+				{store.animalShelter
+									.filter(shelter => filter === "" || shelter.city === filter)
+									.map((iterator, index) =>
 				<div className="col-md-4 mb-4">
+					
 				   
-							<div className="card" >
+						{ <div className="card" >
 								
-							<div className="card-body">
-								<h5 className="card-title mb-2 text-center">{iterator.shelter_name}</h5>
-								<img class="card-img-top" src={`${rutaImagenes}/${iterator.web_url}`} alt="Card image cap"/>
-								<h5 className="card-title mb-2">{iterator.city}</h5>
-							
-																					  
-								<Link to="/animal-shelter-detail" >
-											<button type="button" className="btn btn-primary mb-2" onClick={(event) => actions.getAnimalShelter(iterator.id)} >Ver protectora</button>
-								</Link>
+								<div className="card-body">
+									<h5 className="card-title mb-2 text-center">{iterator.shelter_name}</h5>
+									<img class="card-img-top" src={`${rutaImagenes}/${iterator.img_url}`} alt="Card image cap"/>
+									<h5 className="card-title mb-2">{iterator.city}</h5>
 								
-							{/*} 	<button 
-									className="btn btn-outline-warning"
-									onClick={() => actions.addFavorite(iterator, store.selectedCategory)}
-								>
-									<i className="fas fa-heart"></i> Add to Favorites
-									</button>
-							*/}	
-							
-							</div>
-							</div>
-						</div>
+																						
+									<Link to="/animal-shelter-detail" >
+												<button type="button" className="btn btn-primary mb-2" onClick={(event) => actions.getAnimalShelter(iterator.id)} >Ver protectora</button>
+									</Link>
+									
+								{/*} 	<button 
+										className="btn btn-outline-warning"
+										onClick={() => actions.addFavorite(iterator, store.selectedCategory)}
+									>
+										<i className="fas fa-heart"></i> Add to Favorites
+										</button>
+								*/}	
+								
+								</div>
+							</div>}	
+				</div>
 					
 					)}
 
